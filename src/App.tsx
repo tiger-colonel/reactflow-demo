99d62f5aef1d68e0588076ebbb8ecccfa31b840a
zzc <zhaizhaocheng@corp.netease.com>
2024-11-29 18:11:34 +0800

    feat: undoredo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback } from "react";
 import {
   ReactFlow,
   Background,
@@ -8,35 +8,71 @@ import {
   useNodesState,
   useEdgesState,
   type OnConnect,
-} from '@xyflow/react';
+  type Node,
+  type Edge,
+} from "@xyflow/react";
 
-import '@xyflow/react/dist/style.css';
+import "@xyflow/react/dist/style.css";
 
-import { initialNodes, nodeTypes } from './nodes';
-import { initialEdges, edgeTypes } from './edges';
+import { initialNodes, nodeTypes } from "./nodes";
+import { initialEdges, edgeTypes } from "./edges";
+import { Button, Flex } from "antd";
 
 export default function App() {
-  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const onConnect: OnConnect = useCallback(
     (connection) => setEdges((edges) => addEdge(connection, edges)),
     [setEdges]
   );
+  const handleAdd = () => {
+    console.log("123=========>", 123);
+    const newNode: Node = {
+      id: `node-${Math.random().toString(36).substring(7)}`,
+      type: "default",
+      position: { x: 50, y: 50 },
+      data: { label: "Custom Node" },
+    };
+    setNodes((nodes) => nodes.concat([newNode]));
+  };
+
+  const handleDragEnd = (e, node) => {
+    console.log("nds=========>", node);
+  };
 
   return (
-    <ReactFlow
-      nodes={nodes}
-      nodeTypes={nodeTypes}
-      onNodesChange={onNodesChange}
-      edges={edges}
-      edgeTypes={edgeTypes}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      fitView
-    >
-      <Background />
-      <MiniMap />
-      <Controls />
-    </ReactFlow>
+    <div className="h-screen w-screen">
+      <Flex
+        align="center"
+        justify="space-start"
+        gap={24}
+        className="h-[54px] w-full px-[24px] absolute top-0 z-[99]"
+        style={{
+          background: "#EBF7F8",
+        }}
+      >
+        <Button type="primary" onClick={handleAdd}>
+          添加节点
+        </Button>
+        <Button type="primary">撤销</Button>
+        <Button type="primary">重做</Button>
+      </Flex>
+      <ReactFlow
+        nodes={nodes}
+        // nodeTypes={nodeTypes}
+        // onNodeDragStop={onNodesChange}
+        onNodeDragStop={handleDragEnd}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        edgeTypes={edgeTypes}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
+        <Background />
+        <MiniMap />
+        <Controls />
+      </ReactFlow>
+    </div>
   );
 }
--- src/nodes/index.ts
+++ src/nodes/index.ts
@@ -1,26 +1,32 @@
-import type { NodeTypes } from '@xyflow/react';
+import type { NodeTypes } from "@xyflow/react";
 
-import { PositionLoggerNode } from './PositionLoggerNode';
-import { AppNode } from './types';
+// import { PositionLoggerNode } from "./PositionLoggerNode";
+// import { AppNode } from "./types";
+import type { Node } from "@xyflow/react";
 
-export const initialNodes: AppNode[] = [
-  { id: 'a', type: 'input', position: { x: 0, y: 0 }, data: { label: 'wire' } },
+export const initialNodes: Node[] = [
   {
-    id: 'b',
-    type: 'position-logger',
-    position: { x: -100, y: 100 },
-    data: { label: 'drag me!' },
+    id: "a",
+    type: "default",
+    position: { x: 0, y: 0 },
+    data: { label: "wire" },
   },
-  { id: 'c', position: { x: 100, y: 100 }, data: { label: 'your ideas' } },
-  {
-    id: 'd',
-    type: 'output',
-    position: { x: 0, y: 200 },
-    data: { label: 'with React Flow' },
-  },
+  // {
+  //   id: "b",
+  //   type: "position-logger",
+  //   position: { x: -100, y: 100 },
+  //   data: { label: "drag me!" },
+  // },
+  // { id: 'c', position: { x: 100, y: 100 }, data: { label: 'your ideas' } },
+  // {
+  //   id: 'd',
+  //   type: 'output',
+  //   position: { x: 0, y: 200 },
+  //   data: { label: 'with React Flow' },
+  // },
 ];
 
 export const nodeTypes = {
-  'position-logger': PositionLoggerNode,
+  // "position-logger": PositionLoggerNode,
   // Add any of your custom nodes here!
 } satisfies NodeTypes;
