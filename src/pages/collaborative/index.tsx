e86b1617bdb350ff8b598917cccfad8209588c72
zzc <zzcheng.me@gmail.com>
2025-03-12 23:52:30 +0800

    feat: add todo

---
+++ src/pages/collaborative/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CollaborativePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Collaborative Page</h1>
+      <p>Welcome to the collaborative page!</p>
+    </div>
+  );
+};
+
+export default CollaborativePage;
